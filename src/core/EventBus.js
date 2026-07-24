20365a11c1ed97506ec8ba56e7627ddc52bf008b
sefes3000-dev <sefes3000@gmail.com>
2026-07-24 16:16:45 +0100

    Create EventBus.js

---
+++ src/core/EventBus.js
@@ -0,0 +1,61 @@
+export default class EventBus {
+
+    constructor() {
+
+        this.events = {};
+
+    }
+
+    on(eventName, callback) {
+
+        if (!this.events[eventName]) {
+
+            this.events[eventName] = [];
+
+        }
+
+        this.events[eventName].push(callback);
+
+    }
+
+    off(eventName, callback) {
+
+        if (!this.events[eventName]) return;
+
+        this.events[eventName] = this.events[eventName].filter(
+
+            listener => listener !== callback
+
+        );
+
+    }
+
+    emit(eventName, data = null) {
+
+        if (!this.events[eventName]) return;
+
+        this.events[eventName].forEach(listener => {
+
+            listener(data);
+
+        });
+
+    }
+
+    clear(eventName) {
+
+        if (this.events[eventName]) {
+
+            delete this.events[eventName];
+
+        }
+
+    }
+
+    clearAll() {
+
+        this.events = {};
+
+    }
+
+}

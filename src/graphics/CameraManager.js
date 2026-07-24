d98747187b5fa61f636f10cd4ef67ad87c8d9701
sefes3000-dev <sefes3000@gmail.com>
2026-07-24 23:02:50 +0100

    Update CameraManager.js

--- src/graphics/CameraManager.js
+++ src/graphics/CameraManager.js
@@ -6,13 +6,22 @@ export default class CameraManager {
 
         this.camera = camera;
 
-        this.target = new THREE.Vector3(0, 0, 0);
+        this.target = new THREE.Vector3();
+
+        this.currentPosition = new THREE.Vector3();
+        this.desiredPosition = new THREE.Vector3();
 
         this.defaultPosition = new THREE.Vector3(0, 12, 12);
-
         this.topPosition = new THREE.Vector3(0, 22, 0.01);
 
+        this.followTarget = null;
+
         this.currentMode = "DEFAULT";
+
+        this.smoothSpeed = 5;
+
+        this.minZoom = 6;
+        this.maxZoom = 30;
 
     }
 
@@ -24,31 +33,102 @@ export default class CameraManager {
 
     reset() {
 
-        this.camera.position.copy(this.defaultPosition);
+        this.currentMode = "DEFAULT";
+
+        this.currentPosition.copy(this.defaultPosition);
+
+        this.desiredPosition.copy(this.defaultPosition);
+
+        this.camera.position.copy(this.currentPosition);
 
         this.camera.lookAt(this.target);
-
-        this.currentMode = "DEFAULT";
 
     }
 
     setTopView() {
 
-        this.camera.position.copy(this.topPosition);
-
-        this.camera.lookAt(this.target);
-
         this.currentMode = "TOP";
 
-    }
-
-    zoom(delta) {
-
-        this.camera.position.multiplyScalar(delta);
+        this.desiredPosition.copy(this.topPosition);
 
     }
 
-    update() {
+    setDefaultView() {
+
+        this.currentMode = "DEFAULT";
+
+        this.desiredPosition.copy(this.defaultPosition);
+
+    }
+
+    follow(object3D) {
+
+        this.followTarget = object3D;
+
+    }
+
+    clearFollow() {
+
+        this.followTarget = null;
+
+    }
+
+    zoom(amount) {
+
+        const direction = this.camera.position.clone().normalize();
+
+        let distance = this.camera.position.length();
+
+        distance += amount;
+
+        distance = THREE.MathUtils.clamp(
+            distance,
+            this.minZoom,
+            this.maxZoom
+        );
+
+        this.desiredPosition.copy(
+            direction.multiplyScalar(distance)
+        );
+
+    }
+
+    rotateY(angle) {
+
+        const offset = this.desiredPosition.clone();
+
+        offset.applyAxisAngle(
+            new THREE.Vector3(0,1,0),
+            angle
+        );
+
+        this.desiredPosition.copy(offset);
+
+    }
+
+    update(delta = 0.016) {
+
+        if (this.followTarget) {
+
+            this.target.lerp(
+
+                this.followTarget.position,
+
+                delta * this.smoothSpeed
+
+            );
+
+        }
+
+        this.currentPosition.lerp(
+
+            this.desiredPosition,
+
+            delta * this.smoothSpeed
+
+        );
+
+        this.camera.position.copy(this.currentPosition);
 
         this.camera.lookAt(this.target);
 

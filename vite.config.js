fe597de828286014e4557a36dda4bda9eba0f269
sefes3000-dev <sefes3000@gmail.com>
2026-07-24 16:02:01 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    host: true,
+    port: 5173
+  },
+
+  build: {
+    outDir: "dist",
+    sourcemap: false
+  }
+});

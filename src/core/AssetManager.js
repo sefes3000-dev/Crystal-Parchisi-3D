03127d2827003cf7b0857b1abb95c50b1a3272b5
sefes3000-dev <sefes3000@gmail.com>
2026-07-24 22:54:56 +0100

    Update AssetManager.js

--- src/core/AssetManager.js
+++ src/core/AssetManager.js
@@ -7,25 +7,106 @@ export default class AssetManager {
         this.sounds = new Map();
         this.fonts = new Map();
 
+        this.queue = [];
+
+        this.loaded = false;
+
+        this.loading = false;
+
+    }
+
+    registerTexture(name, loader) {
+
+        this.queue.push({
+            type: "texture",
+            name,
+            loader
+        });
+
+    }
+
+    registerModel(name, loader) {
+
+        this.queue.push({
+            type: "model",
+            name,
+            loader
+        });
+
+    }
+
+    registerSound(name, loader) {
+
+        this.queue.push({
+            type: "sound",
+            name,
+            loader
+        });
+
+    }
+
+    registerFont(name, loader) {
+
+        this.queue.push({
+            type: "font",
+            name,
+            loader
+        });
+
     }
 
     async load() {
 
+        if (this.loaded) return;
+
+        this.loading = true;
+
         console.log("Loading Assets...");
 
-        // سيتم لاحقًا تحميل:
-        // textures
-        // glb models
-        // sounds
-        // fonts
+        for (const asset of this.queue) {
+
+            try {
+
+                const data = await asset.loader();
+
+                switch (asset.type) {
+
+                    case "texture":
+                        this.textures.set(asset.name, data);
+                        break;
+
+                    case "model":
+                        this.models.set(asset.name, data);
+                        break;
+
+                    case "sound":
+                        this.sounds.set(asset.name, data);
+                        break;
+
+                    case "font":
+                        this.fonts.set(asset.name, data);
+                        break;
+
+                }
+
+                console.log(`Loaded ${asset.type}: ${asset.name}`);
+
+            } catch (error) {
+
+                console.error(
+                    `Failed loading ${asset.name}`,
+                    error
+                );
+
+            }
+
+        }
+
+        this.loaded = true;
+
+        this.loading = false;
 
         console.log("Assets Loaded");
-
-    }
-
-    addTexture(name, texture) {
-
-        this.textures.set(name, texture);
 
     }
 
@@ -35,27 +116,87 @@ export default class AssetManager {
 
     }
 
-    addModel(name, model) {
-
-        this.models.set(name, model);
-
-    }
-
     getModel(name) {
 
         return this.models.get(name);
 
     }
 
-    addSound(name, sound) {
-
-        this.sounds.set(name, sound);
-
-    }
-
     getSound(name) {
 
         return this.sounds.get(name);
+
+    }
+
+    getFont(name) {
+
+        return this.fonts.get(name);
+
+    }
+
+    hasTexture(name) {
+
+        return this.textures.has(name);
+
+    }
+
+    hasModel(name) {
+
+        return this.models.has(name);
+
+    }
+
+    hasSound(name) {
+
+        return this.sounds.has(name);
+
+    }
+
+    hasFont(name) {
+
+        return this.fonts.has(name);
+
+    }
+
+    unloadTexture(name) {
+
+        this.textures.delete(name);
+
+    }
+
+    unloadModel(name) {
+
+        this.models.delete(name);
+
+    }
+
+    unloadSound(name) {
+
+        this.sounds.delete(name);
+
+    }
+
+    unloadFont(name) {
+
+        this.fonts.delete(name);
+
+    }
+
+    dispose() {
+
+        this.textures.clear();
+
+        this.models.clear();
+
+        this.sounds.clear();
+
+        this.fonts.clear();
+
+        this.queue = [];
+
+        this.loaded = false;
+
+        this.loading = false;
 
     }
 

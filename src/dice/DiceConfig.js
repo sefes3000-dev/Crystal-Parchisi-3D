7e2a93d4b82d225d293c38359b323e7c8b051071
sefes3000-dev <sefes3000@gmail.com>
2026-07-24 16:45:15 +0100

    Create DiceConfig.js

---
+++ src/dice/DiceConfig.js
@@ -0,0 +1,29 @@
+const DiceConfig = {
+
+    COUNT: 2,
+
+    SIZE: 0.8,
+
+    MASS: 1,
+
+    RESTITUTION: 0.35,
+
+    FRICTION: 0.45,
+
+    LINEAR_DAMPING: 0.2,
+
+    ANGULAR_DAMPING: 0.2,
+
+    START_HEIGHT: 2,
+
+    THROW_FORCE: 8,
+
+    TORQUE_FORCE: 20,
+
+    SLEEP_SPEED: 0.15,
+
+    SLEEP_TIME: 1
+
+};
+
+export default DiceConfig;

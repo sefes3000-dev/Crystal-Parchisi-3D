efc1e199a1de87f48436cc4a7e2425bf8490a51a
sefes3000-dev <sefes3000@gmail.com>
2026-07-25 02:54:00 +0100

    Update BoardCoordinates.js

--- src/board/BoardCoordinates.js
+++ src/board/BoardCoordinates.js
@@ -12,6 +12,13 @@ const BoardCoordinates = {
         YELLOW: [],
         GREEN: [],
         BLUE: []
+    },
+
+    bases: {
+        RED: [],
+        YELLOW: [],
+        GREEN: [],
+        BLUE: []
     }
 
 };
@@ -19,7 +26,69 @@ const BoardCoordinates = {
 function add(x, z) {
 
     BoardCoordinates.mainPath.push(
-        new THREE.Vector3(x * TILE_SIZE, Y, z * TILE_SIZE)
+        new THREE.Vector3(
+            x * TILE_SIZE,
+            Y,
+            z * TILE_SIZE
+        )
     );
 
 }
+
+function home(color, x, z) {
+
+    BoardCoordinates.homePaths[color].push(
+        new THREE.Vector3(
+            x * TILE_SIZE,
+            Y,
+            z * TILE_SIZE
+        )
+    );
+
+}
+
+function base(color, x, z) {
+
+    BoardCoordinates.bases[color].push(
+        new THREE.Vector3(
+            x * TILE_SIZE,
+            Y,
+            z * TILE_SIZE
+        )
+    );
+
+}
+
+/*==========================
+        MAIN PATH
+==========================*/
+
+add(-6,1);
+add(-5,1);
+add(-4,1);
+add(-3,1);
+add(-2,1);
+add(-1,1);
+
+add(-1,2);
+add(-1,3);
+
+add(-2,3);
+add(-3,3);
+
+add(-3,4);
+add(-3,5);
+add(-3,6);
+
+add(-2,6);
+add(-1,6);
+
+add(0,6);
+
+add(1,6);
+
+add(1,5);
+
+add(1,4);
+
+add(1,3);

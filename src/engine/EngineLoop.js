c4cdc8ad0e39aa2f24960c2e020865b561517ee7
sefes3000-dev <sefes3000@gmail.com>
2026-07-25 02:55:33 +0100

    Update EngineLoop.js

--- src/engine/EngineLoop.js
+++ src/engine/EngineLoop.js
@@ -1,8 +1,11 @@
 export default class EngineLoop {
+
     constructor(engine) {
+
         this.engine = engine;
 
         this.running = false;
+
         this.paused = false;
 
         this.frameId = null;
@@ -13,6 +16,12 @@ export default class EngineLoop {
 
         this.maxDelta = 0.05;
 
+        this.targetFPS = 60;
+
+        this.frameDuration = 1000 / this.targetFPS;
+
+        this.accumulator = 0;
+
         this.fps = 0;
 
         this.frameCounter = 0;
@@ -20,54 +29,124 @@ export default class EngineLoop {
         this.fpsTimer = 0;
 
         this.loop = this.loop.bind(this);
+
+        document.addEventListener(
+            "visibilitychange",
+            () => {
+
+                if (document.hidden) {
+
+                    this.pause();
+
+                } else {
+
+                    this.resume();
+
+                }
+
+            }
+
+        );
+
     }
 
     start() {
+
         if (this.running) return;
 
         this.running = true;
+
         this.paused = false;
 
         this.lastTime = performance.now();
 
+        this.accumulator = 0;
+
         this.frameId = requestAnimationFrame(this.loop);
+
     }
 
     stop() {
+
         this.running = false;
 
+        this.paused = false;
+
         if (this.frameId !== null) {
+
             cancelAnimationFrame(this.frameId);
+
             this.frameId = null;
+
         }
+
     }
 
     pause() {
+
         this.paused = true;
+
     }
 
     resume() {
+
         if (!this.running) return;
 
         this.paused = false;
 
         this.lastTime = performance.now();
+
     }
 
     loop(time) {
+
         if (!this.running) return;
 
-        this.delta = (time - this.lastTime) / 1000;
-
-        if (this.delta > this.maxDelta) {
-            this.delta = this.maxDelta;
-        }
+        let elapsed = time - this.lastTime;
 
         this.lastTime = time;
 
-        if (!this.paused) {
+        if (elapsed > 250) {
 
-            this.engine.update(this.delta);
+            elapsed = this.frameDuration;
+
+        }
+
+        this.accumulator += elapsed;
+
+        while (this.accumulator >= this.frameDuration) {
+
+            this.delta = this.frameDuration / 1000;
+
+            if (this.delta > this.maxDelta) {
+
+                this.delta = this.maxDelta;
+
+            }
+
+            if (!this.paused) {
+
+                if (this.engine.beforeUpdate) {
+
+                    this.engine.beforeUpdate(this.delta);
+
+                }
+
+                this.engine.update(this.delta);
+
+                if (this.engine.afterUpdate) {
+
+                    this.engine.afterUpdate(this.delta);
+
+                }
+
+            }
+
+            this.accumulator -= this.frameDuration;
+
+        }
+
+        if (!this.paused) {
 
             this.engine.render();
 
@@ -75,7 +154,7 @@ export default class EngineLoop {
 
         this.frameCounter++;
 
-        this.fpsTimer += this.delta;
+        this.fpsTimer += elapsed / 1000;
 
         if (this.fpsTimer >= 1) {
 
@@ -88,9 +167,27 @@ export default class EngineLoop {
         }
 
         this.frameId = requestAnimationFrame(this.loop);
+
+    }
+
+    setFPS(fps) {
+
+        this.targetFPS = fps;
+
+        this.frameDuration = 1000 / fps;
+
     }
 
     getFPS() {
+
         return this.fps;
+
     }
+
+    isRunning() {
+
+        return this.running;
+
+    }
+
 }
